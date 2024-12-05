e66d4e51bea60dd7d533c233a88852860500efc8
Ashley Sands <ashley.sands01@gmail.com>
2024-12-05 12:44:03 -0500

    add model and setup database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    year_published = models.IntegerField()
+
+    def __str__(self):
+        return self.name
