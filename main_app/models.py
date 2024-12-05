b97a8f8dceaae404b96da5b5a05f8f827b9716bd
Ashley Sands <ashley.sands01@gmail.com>
2024-12-05 13:44:23 -0500

    add review model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+RATINGS = (
+    ('5', '★★★★★'),
+    ('4', '★★★★'),
+    ('3', '★★★'),
+    ('2', '★★'),
+    ('1', '★'),
+)
+
+
+
 class Book(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=250, blank=True)
@@ -14,3 +24,15 @@ class Book(models.Model):
     
     def get_absolute_url(self):
         return reverse('book-detail', kwargs={'book_id': self.id})
+
+class Review(models.Model):
+    comment = models.TextField(max_length=250, blank=True)
+    rating = models.CharField(
+        max_length=1,
+        choices=RATINGS,
+        default=RATINGS[0][0]
+    )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"This books rating is {self.get_rating_display()}"
